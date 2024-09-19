c8c062db50b7f0170d1fc9848a02b82113de5e79
eolecvk <eolecvk@gmail.com>
2024-09-19 22:26:52 +0700

    rm comments from Docker run cmd

--- backend/local/deploy.sh
+++ backend/local/deploy.sh
@@ -95,31 +95,31 @@ if sudo docker ps -a --format '{{.Names}}' | grep -Eq "^opensora-inference$"; th
     sudo docker rm -f opensora-inference || { echo "Failed to remove existing opensora-inference Docker container"; exit 1; }
 fi
 
-# Construct the docker run command
-DOCKER_RUN_CMD="sudo docker run --rm \
-  --env-file /home/ubuntu/text2vid-viewer/.env \
-  -v /home/ubuntu/data:/data \  # Mount the data directory
-  -v ${PROMPT_PATH}:/app/prompts.txt \  # Mount the prompts file
-  -v /home/ubuntu/logs:/app/logs \  # Mount the logs directory
-  opensora-inference \
-  --model ${MODEL} \
-  --prompt-path /app/prompts.txt"
-
-# Echo the constructed command
-echo "Running the following command:"
-echo "$DOCKER_RUN_CMD"
-
-# Execute the command
-eval "$DOCKER_RUN_CMD"
-
-# # Run container opensora-inference with environment variables
-# sudo docker run --rm \
+# # Construct the docker run command
+# DOCKER_RUN_CMD="sudo docker run --rm \
 #   --env-file /home/ubuntu/text2vid-viewer/.env \
 #   -v /home/ubuntu/data:/data \  # Mount the data directory
 #   -v ${PROMPT_PATH}:/app/prompts.txt \  # Mount the prompts file
 #   -v /home/ubuntu/logs:/app/logs \  # Mount the logs directory
 #   opensora-inference \
 #   --model ${MODEL} \
-#   --prompt-path /app/prompts.txt
+#   --prompt-path /app/prompts.txt"
+
+# # Echo the constructed command
+# echo "Running the following command:"
+# echo "$DOCKER_RUN_CMD"
+
+# # Execute the command
+# eval "$DOCKER_RUN_CMD"
+
+# # Run container opensora-inference with environment variables
+sudo docker run --rm \
+  --env-file /home/ubuntu/text2vid-viewer/.env \
+  -v /home/ubuntu/data:/data \
+  -v prompts.txt:/app/prompts.txt \
+  -v /home/ubuntu/logs:/app/logs \
+  opensora-inference \
+  --model opensora-v1-2 \
+  --prompt-path /app/prompts.txt
 
 echo "Deployment script completed successfully."
