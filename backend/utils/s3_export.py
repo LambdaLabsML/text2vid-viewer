cd9e0ce5ead8af4e73cfc8c296ae06df07b361db
eolecvk <eolecvk@gmail.com>
2024-10-19 14:44:07 +0700

    debug prints

--- backend/utils/s3_export.py
+++ backend/utils/s3_export.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import logging
 import glob
 import pandas as pd
 from s3_utils import upload_file_to_s3
@@ -28,8 +27,19 @@ if __name__ == "__main__":
     prompts = df['prompt'].tolist()  # Get the 'prompt' column
     base_prompts = df['base_prompt'].tolist()  # Get the 'base_prompt' column
 
+    print("prompts")
+    print(prompts)
+    print()
+    print("base_prompts")
+    print(base_prompts)
+    print()
+
     # Export to S3
     generated_files = glob.glob(os.path.join("/home/ubuntu/data", '*.mp4'))
+
+    print("generated_files")
+    print(generated_files)
+    print()
     
     for generated_file_path in generated_files:
         # Extract the index from the file path (e.g., "sample_001.mp4" -> 001)
@@ -48,15 +58,23 @@ if __name__ == "__main__":
 
         prompt = prompts[index]  # Retrieve the original prompt based on the index
         base_prompt = base_prompts[index]  # Retrieve the corresponding base_prompt
-
         bucket_name = "text2videoviewer"
-        object_name = f"{model}/{filename}"
+        object_name = f"{model}/{prompt}.mp4"  # Corrected the object name
 
         metadata = {
             "model": model,
             "prompt": prompt,
-            "base_prompt": base_prompt
+            "base_prompt": base_prompt  # Include base_prompt in the metadata
         }
+
+        print("SENDING TO S3")
+        print(f"prompt: {prompt}")
+        print(f"base_prompt: {base_prompt}")
+        print(f"bucket_name: {bucket_name}")
+        print(f"object_name: {object_name}")
+        #print(f"metadata: {metadata}")
+        print()
+
 
         response = upload_file_to_s3(generated_file_path, bucket_name, object_name, metadata)
         if response is not None:
