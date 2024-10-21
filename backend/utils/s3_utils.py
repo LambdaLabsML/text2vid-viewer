e47e8303fd27851fafe9ad2f8dfb78a58f0e67e5
eolecvk <eolecvk@gmail.com>
2024-10-21 15:33:22 +0700

    remove make_filename save when refreshing the csv file

--- backend/utils/s3_utils.py
+++ backend/utils/s3_utils.py
@@ -91,7 +91,7 @@ def update_csv(csv_fpath, bucket_name="text2videoviewer"):
 
         # Remove the file extension to get the prompt
         prompt = filename.rsplit(".", 1)[0]
-        prompt = make_safe_filename(prompt)
+        #prompt = make_safe_filename(prompt)
 
         # No longer removing commas from prompts
         records.append({"model": model, "prompt": prompt, "object_name": obj})
