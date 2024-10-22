411d5fb6751c90388dd8768825c693631d85688f
eolecvk <eolecvk@gmail.com>
2024-10-22 13:26:03 +0700

    use a virtual env

--- run_frontend.sh
+++ run_frontend.sh
@@ -10,17 +10,28 @@ fi
 export $(grep -v '^#' .env | xargs)
 echo "Sourced .env file"
 
-
-# Make sure pip is installed
+# Make sure pip and venv are installed
 if ! command -v pip &> /dev/null; then
     echo "Pip not found. Installing pip"
     sudo apt-get update
-    sudo apt-get install python3-pip -y || { echo "Failed to install pip"; exit 1; }
+    sudo apt-get install python3-pip python3-venv -y || { echo "Failed to install pip or venv"; exit 1; }
 fi
 
-# Install dependencies
-python3 -m pip install --upgrade pip flatbuffers
-python3 -m pip install boto3 python-dotenv pandas || { echo "Failed to install dependencies"; exit 1; }
+# Create a virtual environment if not already created
+if [ ! -d "venv" ]; then
+    python3 -m venv venv || { echo "Failed to create virtual environment"; exit 1; }
+    echo "Created virtual environment"
+fi
+
+# Activate the virtual environment
+source venv/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
+echo "Activated virtual environment"
+
+# Upgrade pip inside the virtual environment
+python3 -m pip install --upgrade pip || { echo "Failed to upgrade pip"; exit 1; }
+
+# Install dependencies in the virtual environment
+python3 -m pip install flatbuffers boto3 python-dotenv pandas || { echo "Failed to install dependencies"; exit 1; }
 echo "Installed dependencies"
 
 # Refresh db.csv
