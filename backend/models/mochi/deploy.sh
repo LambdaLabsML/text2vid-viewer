209c4c7c4224f1eac5df7374f044a2e7ea6d056e
eolecvk <eolecvk@gmail.com>
2024-10-23 19:30:31 +0700

    activate venv and use uv for installing mochi setup

--- backend/models/mochi/deploy.sh
+++ backend/models/mochi/deploy.sh
@@ -1,26 +1,23 @@
 cd /home/ubuntu
-git clone https://github.com/genmoai/models
-cd models 
+git clone https://github.com/genmoai/models mochi
+cd mochi 
 pip install uv
 
 uv venv .venv
 source .venv/bin/activate
-
-uv pip install -e . --no-build-isolation
-
-
-pip install -r requirements.txt
-pip install huggingface_hub[cli]
-
-# Download model weights
-python /home/ubuntu/text2vid-viewer/backend/models/mochi/dl_weights.py
+uv install -e . --no-build-isolation
+.venv/bin/python3 -m pip install -r requirements.txt
+.venv/bin/python3 -m pip install huggingface_hub[cli]
 
 # Patch the inference script
-cp /home/ubuntu/text2vid-viewer/backend/models/cog/inference.py /home/ubuntu/CogVideo/inference/inference.py
+cp /home/ubuntu/text2vid-viewer/backend/models/mochi/inference.py /home/ubuntu/mochi/src/mochi_preview/inference.py
+
+# Download model weights
+.venv/bin/python3 /home/ubuntu/text2vid-viewer/backend/models/mochi/dl_weights.py
+
 
 # Run inference
-python /home/ubuntu/CogVideo/inference/inference.py \
+.venv/bin/python3 /home/ubuntu/mochi/src/mochi_preview/inference.py \
     --prompt_path /home/ubuntu/text2vid-viewer/prompts.txt \
-    --save_dir /home/ubuntu/data/cog \
-    --model_path THUDM/CogVideoX-5b \
-    --generate_type "t2v"
+    --save_dir /home/ubuntu/data/mochi \
+    --model_path THUDM/CogVideoX-5b
