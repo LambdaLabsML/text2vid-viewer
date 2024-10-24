c5d6219110e0a9e1c16c721b9e28ef8406751e79
eolecvk <eolecvk@gmail.com>
2024-10-24 14:44:24 +0700

    rm deprecated arg dl_weights

--- backend/models/mochi/dl_weights.py
+++ backend/models/mochi/dl_weights.py
@@ -1,7 +1,7 @@
 def dl_model():
     from huggingface_hub import snapshot_download
     model_path = '/home/ubuntu/text2vid-viewer/backend/models/mochi/'   # The local directory to save downloaded checkpoint
-    snapshot_download("genmo/mochi-1-preview", local_dir=model_path, local_dir_use_symlinks=False, repo_type='model')
+    snapshot_download("genmo/mochi-1-preview", local_dir=model_path, repo_type='model')
 
 
 if __name__ == "__main__":
